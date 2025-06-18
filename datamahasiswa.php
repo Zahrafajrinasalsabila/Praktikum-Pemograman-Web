e626c26912f5f9a705b0dbfc338c4d9e821994d9
Zahrafajrinasalsabila <zahrafajrinasalsabila@gmail.com>
2025-06-18 18:04:55 +0700

    FUNCTION

---
+++ datamahasiswa.php
@@ -0,0 +1,68 @@
+<?php
+// $nama = "Pitaloka";
+// echo "Hello World $nama" . " Cantik";
+// echo$nama;
+require "function.php";
+$query = "SELECT * From mahasiswa" ;
+$rows = tampildata($query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA MAHASISWA</title>
+</head>
+
+<body>
+
+    <nav>
+        <ul style="list-style-type: none; text-align: center; padding: 0%;">
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+                <a href="home.html">Home</a>
+            </li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+                <a href="about.html">About</a>
+            </li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+                <a href="service.html">Services</a>
+            </li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+                <a href="contact.html">Contact</a>
+            </li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+                <a href="login.html">Login</a>
+            </li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+                <a href="datamahasiswa.php">Data</a>
+            </li>
+        </ul>
+    </nav>
+    <h1 align="center">Data Mahasiswa</h1>
+    <table border="1" cellspacing="0" cellpadding="10px">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+            <th>Alamat</th>
+        </tr>
+        
+        <?php $i = 1; 
+        foreach($rows as $mhs) { ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td><img src="images/mhs/<?$mhs["foto"]?> alt="<?= $mhs[foto] ?> " width"17px" height="17px" style="border-radius: 50%; margin: 0% 10
+                <td><?= $mhs["nama"]; ?> </td>
+                <td><?= $mhs["nim"]; ?> </td>
+                <td><?= $mhs["jurusan"]; ?> </td>
+                <td><?= $mhs["alamat"]; ?> </td>
+            </tr>
+        <?php $i++;} ?>
+    </table>
+</body>
+
+</html>
---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+$koneksi = mysqli_connect("localhost:3307", "root", "", "informatik");
+if (!$koneksi) {
+    die('Koneksi Gagal' . mysqli_connect_error());
+}
+
+function tampildata($query)
+{
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+
+    $rows = [];
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+?>
